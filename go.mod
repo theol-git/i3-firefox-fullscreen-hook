212ac15a1328929e00e401f0bfcbff0d0adb0b29
t.lettermann <t.lettermann@criteo.com>
2024-05-24 17:58:58 +0200

    feat: press F11 on firefox fullscreen

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/theol-git/i3-firefox-fullscreen-hook
+
+go 1.22.3
+
+require (
+	github.com/fstanis/screenresolution v0.0.0-20190527020317-869904d15333
+	go.i3wm.org/i3 v0.0.0-20190720062127-36e6ec85cc5a
+)
+
+require (
+	github.com/BurntSushi/xgb v0.0.0-20160522181843-27f122750802 // indirect
+	github.com/BurntSushi/xgbutil v0.0.0-20160919175755-f7c97cef3b4e // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
+	golang.org/x/sys v0.0.0-20201018230417-eeed37f84f13 // indirect
+)
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+	"time"
+
+	"go.i3wm.org/i3"
+)
+
+func main() {
+	recv := i3.Subscribe(i3.WindowEventType)
+	for recv.Next() {
+		ev := recv.Event().(*i3.WindowEvent)
+		if isFirefoxEnterFullscreenEvent(ev) {
+			time.Sleep(100 * time.Millisecond)
+			cmd := exec.Command("xdotool", "key", "--clearmodifiers", "F11")
+			if err := cmd.Run(); err != nil {
+				log.Fatal(err)
+			}
+			log.Println("Sent cmd")
+		}
+	}
+}
+
+func isFirefoxEnterFullscreenEvent(event *i3.WindowEvent) bool {
+	if event.Container.WindowProperties.Class != "firefox" {
+		return false
+	}
+
+	if event.Change != "fullscreen_mode" {
+		return false
+	}
+
+	workspaceNode := getFocusedWorkspaceNode()
+	if workspaceNode.Rect.X == event.Container.Rect.X && workspaceNode.Rect.Y == event.Container.Rect.Y {
+		return false
+	}
+
+	return true
+}
+
+func getFocusedWorkspaceNode() *i3.Node {
+	tree, err := i3.GetTree()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	focusedWorkplace := tree.Root.FindFocused(func(n *i3.Node) bool {
+		return n.Type == i3.WorkspaceNode
+	})
+	if focusedWorkplace == nil {
+		log.Fatal("could not locate workspace")
+	}
+
+	return focusedWorkplace
+}
